c6b2d4c86e930c1fb7f0b1c0da816bc746ae08ca
Sharandeep reddy <119688069+sharandeepreddy@users.noreply.github.com>
2024-03-11 21:00:30 +0530

    Update final_app.py

--- Deployment/final_app.py
+++ Deployment/final_app.py
@@ -57,28 +57,14 @@ if uploaded_file is not None:
     
   """#### **PERFORM DIMENSINALITY REDUCTION**"""
   #call the dimensinality reduction funciton
-  # ecg_final = ecg.DimensionalReduciton(ecg_1dsignal)
-  # my_expander4 = st.expander(label='Dimensional Reduction')
-  # with my_expander4:
-  #   st.write(ecg_final)
+  ecg_final = ecg.DimensionalReduciton(ecg_1dsignal)
+  my_expander4 = st.expander(label='Dimensional Reduction')
+  with my_expander4:
+    st.write(ecg_final)
   
   """#### **PASS TO PRETRAINED ML MODEL FOR PREDICTION**"""
   #call the Pretrainsed ML model for prediction
-  # ecg_model = ecg.ModelLoad_predict(ecg_final)
-  # my_expander5 = st.expander(label='PREDICTION')
-  # with my_expander5:
-  #   st.write(ecg_model)
- 
-
-  # Assuming ecg_final is defined somewhere before this code snippet
-  # ecg_final = ...
-  
-  # Call ModelLoad_predict function to get the prediction
-  ecg_model = ecg.ModelLoad_predict(ecg_final)
-  
-  # Create an expander to display the prediction
-  with st.expander(label='PREDICTION'):
-      st.write(ecg_model)
-  
-  
-  
+  ecg_model=ecg.ModelLoad_predict(ecg_final)
+  my_expander5 = st.expander(label='PREDICTION')
+  with my_expander5:
+    st.write(ecg_model)
